f357f61a62538071f4dec29dcfdfa4566186a63d
Tehzeeb124 <moonawanmalik06@gmail.com>
2026-04-27 05:07:03 -0700

    Removed duplicate code and fixed port

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,12 +1,13 @@
-const express = require('express'); // 1. Sab se pehle express
-const cors = require('cors');       // 2. Phir cors
+const express = require('express'); 
+const cors = require('cors');       
 const productRoutes = require('./src/routes/ProductRoutes');
 
-const app = express(); // 3. Ab app banayein (Sirf EK BAAR)
+const app = express(); 
 
-app.use(cors()); // 4. Cors enable karein
-app.use(express.json()); // 5. JSON body parser (Sirf EK BAAR)
+app.use(cors()); 
+app.use(express.json()); 
 
 app.use('/products', productRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server chal raha hai port ${PORT} par!`));
